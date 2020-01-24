1ffea799e4518a1ffd9dab1b73c5dfa5456f609a
dbhide <dhanashree.bhide3@gmail.com>
2020-01-24 14:36:28 +0530

    ADDED: Created array to read dictionary values

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 
 declare -A Results
-
+declare -a arrayResult
 read -p "Enter Value of a: " number1
 read -p "Enter Value of b: " number2
 read -p "Enter Value of c: " number3
@@ -11,11 +11,14 @@ result2=`echo "scale=2; $number1*$number2+$number3" | bc`
 result3=`echo "scale=2; $number3+$number1/$number2" | bc`
 result4=`echo "$number1%$number2+$number3" | bc`
 
-
 Results[op1]="$result1"
 Results[op2]="$result2"
 Results[op3]="$result3"
 Results[op4]="$result4"
 
+for (( i=0;i<4;i++ ))
+do
+	arrayResult[i]=${Results[op$((i+1))]}
+done
 
-
+echo ${arrayResult[@]}
